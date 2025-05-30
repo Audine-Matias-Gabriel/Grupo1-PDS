0dca884e2d544d2b4a830bd1956a31ce8065bb55
Matri <matrioxor401@gmail.com>
2025-05-30 15:56:27 -0300

    TDD

--- test_concatenar.py
+++ test_concatenar.py
@@ -4,3 +4,6 @@ def test_concatenar():
     assert concatenar("A,", "B") == "A,B"
     assert concatenar("Ho", "la") == "Hola"
     assert concatenar("He", "llo") == "Hello"
+    print("Todas las pruebas pasaron.")
+
+test_concatenar()
